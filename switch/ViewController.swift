d8b8042596ecf8811dfbcfa6e6905930e6a7bfc1
HassanAfnan <hassanafnan20@gmail.com>
2021-02-22 21:41:18 +0500

    Switch is done

--- switch/ViewController.swift
+++ switch/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBOutlet weak var swt: UISwitch!
+    
+    
+    @IBAction func valueChanged(_ sender: Any) {
+        if swt.isOn {
+            lbl.text = "Switch is Open";
+        }
+        else
+        {
+            lbl.text = "Switch is Off"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
