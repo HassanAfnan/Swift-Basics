ee4a0eda23c51f0b9133a38505a43c2a690b2130
HassanAfnan <hassanafnan20@gmail.com>
2021-02-27 21:34:51 +0500

    final push

--- autocapitalize/ViewController.swift
+++ autocapitalize/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt1: UITextField!
+    
+    
+    @IBOutlet weak var txt2: UITextField!
+    
+    
+    @IBOutlet weak var txt3: UITextField!
+    
+    
+    @IBOutlet weak var txt4: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        txt1.autocapitalizationType = .allCharacters
+        txt2.autocapitalizationType = .none
+        txt3.autocapitalizationType = .sentences
+        txt4.autocapitalizationType = .words
+        
     }
 
 
