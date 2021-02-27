e6d59be54000f1e66177b993d9e83fbb10b61cde
HassanAfnan <hassanafnan20@gmail.com>
2021-02-27 12:56:26 +0500

    Button and action collection

--- buttonandactioncollection/ViewController.swift
+++ buttonandactioncollection/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var btns: [UIButton]!
+    
+//    @IBAction func btnClick(_ sender: UIButton) {
+//        sender.isSelected.toggle()
+//        //sender.isSelected = !sender.isSelected
+//        if sender.isSelected {
+//            sender.setImage(UIImage(named: "home.png") ,for: .normal)
+//        }else{
+//            sender.setImage(UIImage(named: "home(selected).png") ,for: .normal)
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func BtnCollectionClick(_ sender: UIButton) {
+        for btn in btns{
+            btn.isSelected = false;
+        }
+        if sender.tag == 1 {
+            sender.isSelected.toggle()
+            print("Home")
+        }
+        else if sender.tag == 2{
+            sender.isSelected.toggle()
+            print("Rabbit")
+        }
+        else if sender.tag == 3{
+            sender.isSelected.toggle()
+            print("Tortoise")
+        }
+        else if sender.tag == 4{
+            sender.isSelected.toggle()
+            print("Chats")
+        }
+    }
+    
 
 }
 
