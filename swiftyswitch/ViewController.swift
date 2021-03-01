94baeaaae678474b7089e64898227fc50431ba63
HassanAfnan <hassanafnan20@gmail.com>
2021-03-01 23:17:39 +0500

    Final commit

--- swiftyswitch/ViewController.swift
+++ swiftyswitch/ViewController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SwiftySwitchDelegate {
+    
+    func valueChanged(sender: SwiftySwitch) {
+        if switch1.isOn{
+            self.view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        }else{
+            self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    @IBOutlet weak var switch1: SwiftySwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        switch1.delegate = self
         // Do any additional setup after loading the view.
     }
 
