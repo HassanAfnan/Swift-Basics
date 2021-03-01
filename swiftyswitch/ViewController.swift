bee226a589d3830c5d9c7475794bda5dfb73f889
HassanAfnan <hassanafnan20@gmail.com>
2021-03-01 22:55:26 +0500

    Initial Commit

---
+++ swiftyswitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftyswitch
+//
+//  Created by Afnan on 01/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
