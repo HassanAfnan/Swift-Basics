b294460198c5ed5e8880338c699058054376bd40
HassanAfnan <hassanafnan20@gmail.com>
2021-03-08 23:34:25 +0500

    checkbox

--- checkbox/ViewController.swift
+++ checkbox/ViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BEMCheckBoxDelegate {
 
+    @IBOutlet weak var checkBox1: BEMCheckBox!
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkBox1.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    func didTap(_ checkBox: BEMCheckBox) {
+        self.view.backgroundColor = UIColor.black
     }
 
 
